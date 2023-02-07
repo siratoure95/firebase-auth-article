eb1f45af155752d3fc81b4281df5ab21a4efe570
siratoure95 <siratoure95@gmail.com>
2023-02-06 18:52:37 -0800

    added .env

--- src/firebase.js
+++ src/firebase.js
@@ -17,15 +17,25 @@ import {
   addDoc,
 } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
+//   authDomain: "fir-auth-6edd8.firebaseapp.com",
+//   projectId: "fir-auth-6edd8",
+//   storageBucket: "fir-auth-6edd8.appspot.com",
+//   messagingSenderId: "904760319835",
+//   appId: "1:904760319835:web:44fd0d957f114b4e51447e",
+//   measurementId: "G-Q4TYKH9GG7",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
-  authDomain: "fir-auth-6edd8.firebaseapp.com",
-  projectId: "fir-auth-6edd8",
-  storageBucket: "fir-auth-6edd8.appspot.com",
-  messagingSenderId: "904760319835",
-  appId: "1:904760319835:web:44fd0d957f114b4e51447e",
-  measurementId: "G-Q4TYKH9GG7",
+  apiKey: "AIzaSyAdS_AByRBCeuafXcgMwoKJHPKCIk852_8",
+  authDomain: "dlar-app.firebaseapp.com",
+  projectId: "dlar-app",
+  storageBucket: "dlar-app.appspot.com",
+  messagingSenderId: "71318210387",
+  appId: "1:71318210387:web:1cdf71ce550784fa73c3a6",
+  measurementId: "G-65V8QLCRM3"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
